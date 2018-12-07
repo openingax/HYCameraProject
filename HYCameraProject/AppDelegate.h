d285a1d02fbfe148652baeff2ba4ea0b5fa70298
X-LYing <xiely@viomi.com>
2018-12-07 16:02:46 +0800

    Initial Commit

---
+++ HYCameraProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HYCameraProject
+//
+//  Created by 谢立颖 on 2018/12/7.
+//  Copyright © 2018 Viomi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HYCameraProject/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  HYCameraProject
+//
+//  Created by 谢立颖 on 2018/12/7.
+//  Copyright © 2018 Viomi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
